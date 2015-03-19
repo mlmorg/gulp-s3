2b46723d788d4ab5e3619f85267e9edeff6f923e
Matt Morgan <mlmorg@gmail.com>
2015-03-19 15:46:38 -0700

    Add error message for bad status code

--- index.js
+++ index.js
@@ -52,6 +52,7 @@ module.exports = function (aws, options) {
       client.putBuffer(file.contents, uploadPath, headers, function(err, res) {
         if (err || res.statusCode !== 200) {
           gutil.log(gutil.colors.red('[FAILED]', file.path + " -> " + uploadPath));
+          err = err || new Error('Bad status code received from S3: ' + res.statusCode);
           cb(err);
         } else {
           gutil.log(gutil.colors.green('[SUCCESS]', file.path + " -> " + uploadPath));
